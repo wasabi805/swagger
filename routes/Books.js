acd36098ad8e32a57e30698a379ef03a67eca282
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2021-10-22 22:41:57 -0700

    added more details

--- routes/Books.js
+++ routes/Books.js
@@ -1,6 +1,8 @@
 import express from "express";
 import db from "../db/db.js";
 var router = express.Router();
+// Note: the indentation below HAS TO BE synamticlly be correct in order to display in the ui.
+//  see : https://swagger.io/docs/specification/components/
 /**
  * @openapi
  * components:
@@ -10,7 +12,23 @@ var router = express.Router();
  *          properties:
  *              id:
  *                  type: string
- *                  title: string
+ *                  description: an auto generated id
+ *                  example: d5_gK2ci9
+ *              title:
+ *                  type: string
+ *                  description: title of the series
+ *                  example: The Uncanny X-Men
+ *              issueNumber:
+ *                  type: string
+ *                  description: the issue number of the title series
+ *              artist:
+ *                  type: string
+ *                  description: name of the author
+ *                  example: jim lee
+ *              writer:
+ *                  type: string
+ *                  description: name of the story creator
+ *                  example: Garth Enis
  */
 router.get("/", function (req, res) {
     var data = db.data;
--- routes/Books.ts
+++ routes/Books.ts
@@ -2,10 +2,8 @@ import express from "express"
 import db from "../db/db.js";
 const router = express.Router();
 
-
-
-// Note: the indentation here has to synamticlly be correct in order to display in the ui.
-//see : https://swagger.io/docs/specification/components/
+// Note: the indentation below HAS TO BE synamticlly be correct in order to display in the ui.
+//  see : https://swagger.io/docs/specification/components/
 /**
  * @openapi
  * components:
@@ -15,11 +13,24 @@ const router = express.Router();
  *          properties:
  *              id:
  *                  type: string
- *                  title: string
- */
-               
- 
-
+ *                  description: an auto generated id
+ *                  example: d5_gK2ci9
+ *              title: 
+ *                  type: string
+ *                  description: title of the series
+ *                  example: The Uncanny X-Men
+ *              issueNumber: 
+ *                  type: string
+ *                  description: the issue number of the title series
+ *              artist:
+ *                  type: string
+ *                  description: name of the author
+ *                  example: jim lee
+ *              writer:
+ *                  type: string
+ *                  description: name of the story creator
+ *                  example: Garth Enis
+ */            
 router.get("/", (req, res) => {
     const data = db.data
     data?.books.push({id: 'you' , name: 'someoneElse'})
@@ -32,7 +43,5 @@ router.get("/somewhere", (req, res) => {
     console.log('you can see db persisits at different route' , data?.books)
     res.send( `you made it somewhere else!` )
 })
-
-
 
 export default router
