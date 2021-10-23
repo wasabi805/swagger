40b8a8a6bcf309474c8627c4febc53f3fd1b7df4
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2021-10-22 20:03:14 -0700

    set up swaggerUi to use openapi.yml instead of options obj.

--- index.mjs
+++ index.mjs
@@ -1,51 +1,32 @@
 import dotenv from "dotenv"
-
+dotenv.config()
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 
-import swaggerUI from "swagger-ui-express";
-import swaggerJsDoc from 'swagger-jsdoc';
-
-dotenv.config()
-//import database
-import appReq from './db/db.ts'
-console.log(appReq)
-
 //Routes
 import booksRouter from './routes/Books.ts';
 
-// swagger options
-const options={
-    definition:{
-        openapi: "3.0.0",
-        info:{
-            title: "Comics API",
-            version: "1.0.0",
-            description: "A simple Express Server set up with Swagger"
-        }
-    },
 
-    //list for all servers you can run swagger against
-    servers:[{
-        url: "http://localhost:4000"
-    }],
-
-    apis: ["./routes/*.js"]
-}
-
-const specs = swaggerJsDoc(options)
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs'; // you need this load swagger options from yml file 
+const swaggerDocument = YAML.load("./openapi.yml")
+console.log(swaggerDocument)
 
 // EXPRESS
 const app = express()
 
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const spec = swaggerJsDoc(swaggerDocument)
 
 const PORT = process.env.PORT || 4000;
 
 app.use(
     '/api-docs',  //route
-    swaggerUI.serve, // run serve as callback
-    swaggerUI.setup(specs)) // specify the specs that will build the UI
+    swaggerUi.serve, // run serve as callback
+    swaggerUi.setup(spec)) // specify the specs that will build the UI
 
 //Middlewares
 app.use(cors()); 
--- routes/Books.js
+++ routes/Books.js
@@ -4,12 +4,12 @@ var router = express.Router();
 router.get("/", function (req, res) {
     var data = db.data;
     data === null || data === void 0 ? void 0 : data.books.push({ id: 'you', name: 'someoneElse' });
-    console.log('YEEEEE', data === null || data === void 0 ? void 0 : data.books);
+    console.log('newBook Added', data === null || data === void 0 ? void 0 : data.books);
     res.send("you made it!");
 });
 router.get("/somewhere", function (req, res) {
     var data = db.data;
-    console.log('somewhere else', data === null || data === void 0 ? void 0 : data.books);
+    console.log('you can see db persisits at different route', data === null || data === void 0 ? void 0 : data.books);
     res.send("you made it somewhere else!");
 });
 export default router;
